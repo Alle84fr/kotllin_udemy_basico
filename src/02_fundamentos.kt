ae11807ce6f99d70c3d202fad1bc9e0715f39d6e
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-06-25 16:18:49 -0300

    add files 01_introducao 02_fun_var_val 02_fundamentos 03_var_sint 04_tipo prim

---
+++ src/02_fundamentos.kt
@@ -0,0 +1,39 @@
+/* ir na pasta src onde está os arquivos
+programa inicia no método main
+funções = fun
+ */
+
+fun main (args: Array<String>){
+    println("Saluton Mondo")
+
+    System.out.println("Hello Cód Java do mundo")
+
+}
+
+/* depois apertar play - parte de cima, seta verde
+main = nome da função
+iniciar o parâmetros ()
+args = argumentos
+no formato array
+com tipo string
+
+                                      ---------- ATENÇÃO --------------------------------------------
+
+                                    print = printa na mesma linha, em linha (ver como ficou ao dar play
+
+                                    * USAR println - que pula a lina (tipo \ln do python)
+                                      -----------------------------------------------------------------
+
+array = estrutura de dados que armazena elemntos DO MESMO TIPO, eficiêntes para operações numéricas
+de grande escala
+usase com:
+apenas números
+prcessamento numérico interno ( machine learnig, análise de dados)
+consome menos espaços
+
+obs
+lista[] | list[] = mutável, ordenada, índice inica no 0
+tuplas() | tuple() = imutáveis, ordenadas
+dicionário {chave:valor} | dict{} = mutável, não ordenado (chave e valor)
+conjunto {} | set{} = mutável, não ordenado, elemento único (não permite duplicados)
+ */
