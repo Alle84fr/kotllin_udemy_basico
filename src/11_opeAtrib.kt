e1e87aea22d1435afcd29c6c12b6432622c63484
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-30 12:05:23 -0300

    add 11_operador atribuição

---
+++ src/11_opeAtrib.kt
@@ -0,0 +1,23 @@
+fun main(args: Array<String>){
+
+    /*
+    += mais igual
+    -= menor igual
+    /= divisão igual
+    *= multiplicação igual
+
+    ps: atalho sout do java funcona no .kt
+     */
+
+    var x = 1
+    // x = x + 2 == x+=2
+    println(x)
+    x += 2
+    println(x)
+    x *= 3
+    println(x)
+    x /= 9
+    println(x)
+    x -= 1
+    println(x)
+}
