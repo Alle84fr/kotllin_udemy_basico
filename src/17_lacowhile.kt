c0b080908273e0f2de1a7c0b3e5f3a0fba9e1334
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-30 21:24:40 -0300

    add 17_while

--- src/17_lacowhile.kt
+++ src/17_lacowhile.kt
@@ -4,4 +4,13 @@ fun main(args: Array<String>){
     Estrutura de repetição While
 
      */
+
+    val num = arrayListOf<Int>()
+    var tam = 0
+
+    //inicia com 1 e vai até 10, quando der 11 é false e sai do loop
+    while (num.size != 10){
+        num.add(tam ++)
+        println(num.size)
+    }
 }
