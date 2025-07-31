c165bbd5bbf205d971cc375b229c848427a61329
Alessandra F Rigonatti <alessandra.rigonatti@aluno.faculdadeimpacta.com.br>
2025-07-31 10:29:33 -0300

    add 18_do while

---
+++ src/19_for.kt
@@ -0,0 +1,17 @@
+
+fun main(args: Array<String>){
+
+    /*
+    laço de repetição
+    para cada i na variável ...
+    pega 1° item da variável e passa para o i (este i é muito usado no .py)
+     */
+
+    var ind = 0
+    val cor = arrayListOf("Blue", "Blu", "Azul", "Orange","Arancione", "Laranja")
+    for (i in cor){
+
+        println("cor[$ind]=$i | i= $i")
+        ind ++
+    }
+}
